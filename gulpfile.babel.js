9c79225495b74a676d90e8ccf630aeec27c5a564
Jason Bosco <mail@jasonbos.co>
2018-03-15 07:58:17 -0700

    Added gulp build

---
+++ gulpfile.babel.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import gulp from 'gulp'
+import browserify from 'browserify'
+import source from 'vinyl-source-stream'
+import buffer from 'vinyl-buffer'
+import sourcemaps from 'gulp-sourcemaps'
+import uglify from 'gulp-uglify'
+import del from 'del'
+
+gulp.task('build', function () {
+  return browserify({
+    entries: './lib/Typesense.js',
+    debug: true
+  }).transform('babelify', {presets: ['env']})
+    .bundle()
+    .pipe(source('Typesense.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist/js/'))
+})
+
+gulp.task('clean', function () {
+  return del([
+    'dist/**'
+  ])
+})
