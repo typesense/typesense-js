3d1ff478efaf306e42347e1c2f4b82a39523c41f
Jason Bosco <mail@jasonbos.co>
2018-03-17 23:11:02 -0700

    Fix config validation

---
+++ test/Configuration.spec.js
@@ -0,0 +1,47 @@
+import chai from 'chai'
+import TypesenseClient from '../lib/Typesense/Client'
+
+let expect = chai.expect
+
+describe('Typesense', function () {
+  let typesense
+  beforeEach(function () {
+    typesense = new TypesenseClient({
+      'masterNode': {
+        'host': 'master',
+        'port': '8108',
+        'protocol': 'http',
+        'apiKey': 'abcd'
+      },
+      'readReplicaNodes': [{
+        'host': 'read-replica',
+        'port': '8108',
+        'protocol': 'http',
+        'apiKey': 'abcd'
+      }],
+      'timeoutSeconds': 10
+    })
+  })
+
+  it('throws an error if there is a missing config value in the masterNode', function (done) {
+    delete typesense.configuration.masterNode.host
+
+    expect(() => {
+      typesense.configuration.validate()
+    })
+      .to.throw('Missing required parameters in masterNode')
+
+    done()
+  })
+
+  it('throws an error if there is a missing config value in the readReplicaNodes', function (done) {
+    delete typesense.configuration.readReplicaNodes[0].protocol
+
+    expect(() => {
+      typesense.configuration.validate()
+    })
+      .to.throw('Missing required parameters in one of readReplicaNodes')
+
+    done()
+  })
+})
