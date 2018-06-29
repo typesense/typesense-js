343748beda807b2c5bdd5c5abf18a1bd5d47d8d1
Devin Pastoor <devin.pastoor@gmail.com>
2018-06-29 09:44:18 -0400

    format

--- src/Typesense/ApiCall.js
+++ src/Typesense/ApiCall.js
@@ -4,13 +4,13 @@ import axios from 'axios'
 const APIKEYHEADERNAME = 'X-TYPESENSE-API-KEY'
 
 class ApiCall {
-  constructor (configuration) {
+  constructor(configuration) {
     this._configuration = configuration
     this._defaultNode = 'master'
     this._defaultNodeIndex = 0
   }
 
-  _uriFor (endpoint, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
+  _uriFor(endpoint, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
     if (node === 'readReplica') {
       return `${this._configuration.readReplicaNodes[nodeIndex].protocol}://${this._configuration.readReplicaNodes[nodeIndex].host}:${this._configuration.readReplicaNodes[nodeIndex].port}${this._configuration.readReplicaNodes[nodeIndex].path}${endpoint}`
     } else {
@@ -18,7 +18,7 @@ class ApiCall {
     }
   }
 
-  _apiKey (node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
+  _apiKey(node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
     if (node === 'readReplica') {
       return this._configuration.readReplicaNodes[nodeIndex].apiKey
     } else {
@@ -26,7 +26,7 @@ class ApiCall {
     }
   }
 
-  _defaultHeaders (node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
+  _defaultHeaders(node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
     let defaultHeaders = {}
     defaultHeaders[APIKEYHEADERNAME] = this._apiKey(node, nodeIndex)
     defaultHeaders['Content-Type'] = 'application/json'
@@ -34,20 +34,22 @@ class ApiCall {
     return defaultHeaders
   }
 
-  get (endpoint, parameters = {}, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
+  get(endpoint, parameters = {}, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
     return this.performRequest('get', endpoint, parameters, undefined, undefined, node, nodeIndex)
   }
 
-  delete (endpoint, parameters = {}, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
+  delete(endpoint, parameters = {}, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
     return this.performRequest('delete', endpoint, parameters, undefined, undefined, node, nodeIndex)
   }
 
-  post (endpoint, parameters = {}) {
+  post(endpoint, parameters = {}) {
     return this.performRequest('post', endpoint, undefined, parameters, undefined, 'master')
   }
 
-  performRequest (requestType, endpoint, queryParameters = {}, bodyParameters = {}, additionalHeaders = {}, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
-    this._configuration.validate()
+  performRequest(requestType, endpoint, queryParameters = {}, bodyParameters = {}, additionalHeaders = {}, node = this._defaultNode, nodeIndex = this._defaultNodeIndex) {
+    this
+      ._configuration
+      .validate()
 
     const requestOptions = {
       method: requestType,
@@ -57,29 +59,27 @@ class ApiCall {
       data: bodyParameters
     }
 
-    return axios(requestOptions)
-      .then((response) => {
-        return Promise.resolve(response.data)
-      })
-      .catch((error) => {
-        if (requestType === 'get') {
-          if (node === 'master' && this._configuration.readReplicaNodes.length > 0) {
-            return this.performRequest(requestType, endpoint, queryParameters, bodyParameters, additionalHeaders, 'readReplica', 0)
-          } else if (node === 'readReplica') {
-            if (nodeIndex >= (this._configuration.readReplicaNodes.length - 1)) {
-              // error, but we'll let the code outside the if...else return the error
-            } else {
-              return this.performRequest(requestType, endpoint, queryParameters, bodyParameters, additionalHeaders, node, nodeIndex + 1)
-            }
+    return axios(requestOptions).then((response) => {
+      return Promise.resolve(response.data)
+    }).catch((error) => {
+      if (requestType === 'get') {
+        if (node === 'master' && this._configuration.readReplicaNodes.length > 0) {
+          return this.performRequest(requestType, endpoint, queryParameters, bodyParameters, additionalHeaders, 'readReplica', 0)
+        } else if (node === 'readReplica') {
+          if (nodeIndex >= (this._configuration.readReplicaNodes.length - 1)) {
+            // error, but we'll let the code outside the if...else return the error
+          } else {
+            return this.performRequest(requestType, endpoint, queryParameters, bodyParameters, additionalHeaders, node, nodeIndex + 1)
           }
         }
+      }
 
-        let responseMessage = ''
-        if (error.response !== undefined) {
-          responseMessage = ` - ${error.response.request.path} - ${error.response.data.message}`
-        }
-        return Promise.reject(new Error(`${error.message}${responseMessage}`))
-      })
+      let responseMessage = ''
+      if (error.response !== undefined) {
+        responseMessage = ` - ${error.response.request.path} - ${error.response.data.message}`
+      }
+      return Promise.reject(new Error(`${error.message}${responseMessage}`))
+    })
   }
 }
 
