1d8daf3809ac1ac3cd3fce5fb4a6266975236479
Nick Kamer <nick@ziaomedia.com>
2021-05-27 02:06:21 +0200

    feat: almost full typings for search

--- src/Typesense/RequestWithCache.ts
+++ src/Typesense/RequestWithCache.ts
@@ -1,8 +1,22 @@
 export default class RequestWithCache {
   private responseCache: Record<string, any> = {}
 
-  async perform(requestContext, requestFunction, requestFunctionArguments, { cacheResponseForSeconds = 2 * 60 } = {}) {
+  // Todo: should probably be passed a callback instead, or an apiCall instance. Types are messy this way
+  // Todo: cache is only cleared upon a subsequent - identical - call. add a timeout for other cases
+  // Todo: instead of specifying how long to cache a response for, i think it makes more sense to set a maxAge
+  async perform<T extends any>(
+    requestContext: any,
+    requestFunction: (...params: any) => unknown,
+    requestFunctionArguments: any[],
+    {
+      cacheResponseForSeconds = 2 * 60
+    }: {
+      cacheResponseForSeconds: number
+    }
+  ): Promise<T> {
     // Don't store any responses if cache is disabled
+
+    // Todo: this still leaves an older cache in-tact. Is that desirable? Might as well update or delete it?
     if (cacheResponseForSeconds <= 0) {
       return requestFunction.call(requestContext, ...requestFunctionArguments)
     }
@@ -23,6 +37,6 @@ export default class RequestWithCache {
       requestTimestamp: Date.now(),
       response
     }
-    return response
+    return response as T
   }
 }
