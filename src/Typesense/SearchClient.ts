99c7df5b1632aaae732bde19bde065de3b52d951
Damian C <damian.cantu@nbkc.com>
2021-10-19 21:30:11 -0500

    PR Comments. Adjusted operations on SearchOnlyCollection.

--- src/Typesense/SearchClient.ts
+++ src/Typesense/SearchClient.ts
@@ -11,7 +11,7 @@ export default class SearchClient {
   private readonly individualCollections: Record<string, SearchOnlyCollection>
 
   constructor(options: ConfigurationOptions) {
-    const shouldSendApiKeyAsQueryParam = (options['apiKey'] || '').length < 2000;
+    const shouldSendApiKeyAsQueryParam = (options['apiKey'] || '').length < 2000
     if (shouldSendApiKeyAsQueryParam) {
       options['sendApiKeyAsQueryParam'] = true
     }
@@ -25,7 +25,6 @@ export default class SearchClient {
   collections<TDocumentSchema extends DocumentSchema = {}>(
     collectionName: string
   ): SearchOnlyCollection<TDocumentSchema> | SearchOnlyCollection {
-
     if (!collectionName) {
       throw new Error(
         'Typesense.SearchClient only supports search operations, so the collectionName that needs to ' +
--- src/Typesense/SearchOnlyCollection.ts
+++ src/Typesense/SearchOnlyCollection.ts
@@ -1,47 +1,15 @@
 import { DocumentSchema, SearchableDocuments } from './Documents'
 import ApiCall from './ApiCall'
-import { ObjectNotFound } from './Errors'
-import Collections from './Collections'
-import { CollectionSchema } from './Collection'
 import { SearchOnlyDocuments } from './SearchOnlyDocuments'
-import { ReadOnlyDocument } from './ReadOnlyDocument'
 
 export class SearchOnlyCollection<T extends DocumentSchema = {}> {
   private readonly _documents: SearchableDocuments<T>
-  private individualDocuments: Record<string, ReadOnlyDocument<T>> = {}
 
   constructor(private readonly name: string, private readonly apiCall: ApiCall, private readonly configuration: any) {
     this._documents = new SearchOnlyDocuments(this.name, this.apiCall, this.configuration)
   }
 
-  async retrieve(): Promise<CollectionSchema> {
-    return await this.apiCall.get<CollectionSchema>(this.endpointPath())
-  }
-
-  async exists(): Promise<boolean> {
-    try {
-      await this.retrieve()
-      return true
-    } catch (e) {
-      if (e instanceof ObjectNotFound) return false
-      throw e
-    }
-  }
-
-  documents(): SearchableDocuments<T>
-  documents(documentId: string): SearchableDocuments<T>
-  documents(documentId?: string): ReadOnlyDocument<T> | SearchableDocuments<T> {
-    if (!documentId) {
-      return this._documents
-    } else {
-      if (this.individualDocuments[documentId] === undefined) {
-        this.individualDocuments[documentId] = new ReadOnlyDocument(this.name, documentId, this.apiCall)
-      }
-      return this.individualDocuments[documentId]
-    }
-  }
-
-  private endpointPath(): string {
-    return `${Collections.RESOURCEPATH}/${this.name}`
+  documents(): SearchableDocuments<T> {
+    return this._documents
   }
 }
