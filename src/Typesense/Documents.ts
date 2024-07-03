be432e3533973c068284217140e115fb4685cfaa
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-07-03 13:24:17 +0300

    feat: add type for optional array search params
    Introduces a new utility type, ArrayableSearchParams, and a constant,
    searchParamsArrayKeys. The ArrayableSearchParams typefilters the
    properties of the SearchParams type, selecting only thosewhose type is
    an array. The searchParamsArrayKeys constant is anobject that indicates
    which parameters are expected to be arrays. Thisenhancement provides a
    compile-time check for array parameters,improving type safety and code
    readability.

--- src/Typesense/Documents.ts
+++ src/Typesense/Documents.ts
@@ -35,6 +35,49 @@ export interface SearchParamsWithPreset extends Partial<SearchParams> {
 }
 
 type OperationMode = "off" | "always" | "fallback";
+
+/**
+ * Filters the properties of the `SearchParams` type, selecting only those whose type is an array.
+ * This utility type creates a new type by iterating over each property (`K`) of `SearchParams`.
+ * If the property type is an array (`unknown[]`), it is included in the resulting type with its original type.
+ * Otherwise, the property is excluded.
+ *
+ **/
+export type ArrayableSearchParams = {
+  [K in keyof SearchParams as SearchParams[K] extends infer R
+    ? R extends unknown[]
+      ? K
+      : never
+    : never]: SearchParams[K];
+};
+
+/**
+ * Defines a constant `searchParamsArrayKeys` that explicitly satisfies the structure required by `ArrayableSearchParams`.
+ * Each key in `searchParamsArrayKeys` corresponds to a property name in `ArrayableSearchParams` and is set to `true`.
+ * This object is used to indicate which parameters are expected to be arrays in the context where `ArrayableSearchParams` is applied.
+ * The `satisfies` keyword ensures that `searchParamsArrayKeys` covers all properties defined by `ArrayableSearchParams`,
+ * enforcing a compile-time check that each key in `searchParamsArrayKeys` matches a property in `ArrayableSearchParams`.
+ *
+ **/
+export const searchParamsArrayKeys = {
+  exclude_fields: true,
+  facet_by: true,
+  group_by: true,
+  hidden_hits: true,
+  highlight_fields: true,
+  highlight_full_fields: true,
+  pinned_hits: true,
+  query_by: true,
+  sort_by: true,
+  include_fields: true,
+  infix: true,
+  num_typos: true,
+  prefix: true,
+  query_by_weights: true,
+} satisfies Required<{
+  [K in keyof ArrayableSearchParams]: true;
+}>;
+
 export interface SearchParams {
   // From https://typesense.org/docs/latest/api/documents.html#arguments
   q?: string;
