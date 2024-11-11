cfcaf9eecc4913c2789a926ec4b05ab26ee3d268
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-11-11 14:34:59 +0200

    refactor(utils): improve type safety for array param normalization
    * add runtime type predicates for array value verification
    * rewrite normalizeArrayableParams with proper generics
    * implement Prettify utility type for cleaner type inference
    * add detailed error messages for failed normalizations
    * add early return for non-array cases

--- src/Typesense/Utils.ts
+++ src/Typesense/Utils.ts
@@ -1,14 +1,50 @@
-import { SearchParams, arrayableParams } from "./Documents";
+import { arrayableParams } from "./Documents";
+import type {
+  UnionArrayKeys,
+  ExtractBaseTypes,
+  SearchParams,
+} from "./Documents";
 
-export function normalizeArrayableParams(params: SearchParams): SearchParams {
-  const normalizedParams = { ...params };
-  
-  const arrayableKeys = Object.keys(arrayableParams)
-    .filter(key => key in normalizedParams && Array.isArray(normalizedParams[key]));
-  
-  arrayableKeys.forEach(key => {
-    normalizedParams[key] = normalizedParams[key].join(",");
-  });
-  
-  return normalizedParams;
+function hasNoArrayValues<T extends SearchParams>(
+  params: T | ExtractBaseTypes<T>,
+): params is ExtractBaseTypes<T> {
+  return Object.keys(arrayableParams)
+    .filter((key) => params[key] !== undefined)
+    .every((key) => isNonArrayValue(params[key]));
 }
+
+export function normalizeArrayableParams<T extends SearchParams>(
+  params: T,
+): Prettify<ExtractBaseTypes<T>> {
+  const result = { ...params };
+
+  const transformedValues = Object.keys(arrayableParams)
+    .filter((key) => Array.isArray(result[key]))
+    .map((key) => {
+      result[key] = result[key].join(",");
+      return key;
+    });
+
+  if (!transformedValues.length && hasNoArrayValues(result)) {
+    return result;
+  }
+
+  if (!hasNoArrayValues(result)) {
+    throw new Error(
+      `Failed to normalize arrayable params: ${JSON.stringify(result)}`,
+    );
+  }
+
+  return result;
+}
+
+function isNonArrayValue<T extends SearchParams, K extends UnionArrayKeys<T>>(
+  value: T[K] | ExtractBaseTypes<T>[K],
+): value is ExtractBaseTypes<T>[K] {
+  return !Array.isArray(value);
+}
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+  // eslint-disable-next-line @typescript-eslint/ban-types
+} & {};
