0531afb8bd9802987e16fd5decf0d98884af4180
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-01-22 20:16:06 +0200

    fix(operations): fix order of function vars

--- src/Typesense/Operations.ts
+++ src/Typesense/Operations.ts
@@ -6,9 +6,9 @@ export default class Operations {
   constructor(private apiCall: ApiCall) {}
 
   async perform(
-    queryParameters: Record<string, any> = {},
     // eslint-disable-next-line @typescript-eslint/ban-types -- Can't use `object` here, it needs to intersect with `{}`
     operationName: "vote" | "snapshot" | "cache/clear" | (string & {}),
+    queryParameters: Record<string, any> = {},
   ): Promise<any> {
     return this.apiCall.post(
       `${RESOURCEPATH}/${operationName}`,
