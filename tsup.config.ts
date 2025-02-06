ae7de6cd0bd21a1ab26cdedf8ef6d09e659293be
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-06 17:04:10 +0200

    build: add unminified version of browser dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,37 +3,52 @@ import type { Options } from "tsup";
 import browserList from "browserslist-to-esbuild";
 import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
 
+const baseOptions: Options = {
+  target: browserList(["defaults"]) as Options["target"],
+  entry: {
+    typesense: "src/browser.ts",
+  },
+  clean: true,
+  format: ["cjs", "esm"],
+  outDir: "dist",
+  outExtension({ format }) {
+    return {
+      js: `.min.${format === "cjs" ? "js" : "mjs"}`,
+    };
+  },
+  splitting: true,
+  platform: "browser",
+  esbuildOptions(options) {
+    options.mainFields = ["browser", "module", "main"];
+    options.treeShaking = true;
+  },
+  esbuildPlugins: [
+    nodeModulesPolyfillPlugin({
+      modules: {
+        crypto: "empty",
+        http: "empty",
+        https: "empty",
+      },
+    }),
+  ],
+};
+
 export default defineConfig([
   {
-    target: browserList(["defaults"]) as Options["target"],
-    entry: {
-      typesense: "src/browser.ts",
-    },
-    sourcemap: true,
-    clean: true,
-    format: ["cjs", "esm"],
-    dts: true,
-    outDir: "dist",
+    ...baseOptions,
     minify: true,
+    sourcemap: true,
+    dts: true,
+  },
+  {
+    ...baseOptions,
+    minify: false,
+    sourcemap: false,
+    dts: false,
     outExtension({ format }) {
       return {
-        js: `.min.${format === "cjs" ? "js" : "mjs"}`,
+        js: `.${format === "cjs" ? "js" : "mjs"}`,
       };
     },
-    splitting: true,
-    platform: "browser",
-    esbuildOptions(options) {
-      options.mainFields = ["browser", "module", "main"];
-      options.treeShaking = true;
-    },
-    esbuildPlugins: [
-      nodeModulesPolyfillPlugin({
-        modules: {
-          crypto: "empty",
-          http: "empty",
-          https: "empty",
-        },
-      }),
-    ],
   },
 ]);
