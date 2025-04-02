bb0825f9ed03440be3b3fd754bcac606a0ea9b22
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 17:24:23 +0300

    refactor(multisearch): improve type definitions and stream handling
    - Create `BaseMultiSearchRequestSchema` to share common properties
    - Add dedicated stream configuration interfaces for union and results
    - Move stream configuration from individual searches to common parameters
    - Simplify request processing by removing explicit stream extraction
    - Fix generic type constraints for better type safety

--- src/Typesense/MultiSearch.ts
+++ src/Typesense/MultiSearch.ts
@@ -3,44 +3,45 @@ import Configuration from "./Configuration";
 import RequestWithCache from "./RequestWithCache";
 import {
   DocumentSchema,
+  ExtractBaseTypes,
   SearchParams,
   SearchParamsWithPreset,
   SearchResponse,
   SearchResponseRequestParams,
 } from "./Documents";
 import { normalizeArrayableParams } from "./Utils";
+import { BaseStreamConfig } from "./Configuration";
 
 const RESOURCEPATH = "/multi_search";
 
-export interface MultiSearchRequestSchema<T extends DocumentSchema>
-  extends SearchParams<T> {
+type BaseMultiSearchRequestSchema = {
   collection?: string;
   rerank_hybrid_matches?: boolean;
   "x-typesense-api-key"?: string;
+};
+
+export type MultiSearchRequestSchema<T extends DocumentSchema> =
+  BaseMultiSearchRequestSchema & Omit<SearchParams<T>, "streamConfig">;
+
+export type MultiSearchRequestWithPresetSchema<T extends DocumentSchema> =
+  BaseMultiSearchRequestSchema &
+    Omit<SearchParamsWithPreset<T>, "streamConfig">;
+
+interface SearchesMultiSearchesRequestSchema<T extends DocumentSchema> {
+  searches: (
+    | MultiSearchRequestSchema<T>
+    | MultiSearchRequestWithPresetSchema<T>
+  )[];
 }
 
-export interface MultiSearchRequestWithPresetSchema<T extends DocumentSchema>
-  extends SearchParamsWithPreset<T> {
-  collection?: string;
-  "x-typesense-api-key"?: string;
-}
-
-export interface MultiSearchRequestsWithUnionSchema<T extends DocumentSchema> {
+export interface MultiSearchRequestsWithUnionSchema<T extends DocumentSchema>
+  extends SearchesMultiSearchesRequestSchema<T> {
   union: true;
-  searches: (
-    | MultiSearchRequestSchema<T>
-    | MultiSearchRequestWithPresetSchema<T>
-  )[];
 }
 
-export interface MultiSearchRequestsWithoutUnionSchema<
-  T extends DocumentSchema,
-> {
+export interface MultiSearchRequestsWithoutUnionSchema<T extends DocumentSchema>
+  extends SearchesMultiSearchesRequestSchema<T> {
   union?: false | undefined;
-  searches: (
-    | MultiSearchRequestSchema<T>
-    | MultiSearchRequestWithPresetSchema<T>
-  )[];
 }
 
 export type MultiSearchRequestsSchema<T extends DocumentSchema> =
@@ -58,13 +59,43 @@ export type MultiSearchResponse<
     T[number]
   >,
 > =
-  R extends MultiSearchRequestsWithUnionSchema<any>
+  R extends MultiSearchRequestsWithUnionSchema<T[number]>
     ? UnionSearchResponse<T[number]>
     : {
         results: { [Index in keyof T]: SearchResponse<T[Index]> } & {
           length: T["length"];
         };
       };
+
+export interface MultiSearchUnionStreamConfig<T extends DocumentSchema>
+  extends BaseStreamConfig {
+  onComplete?: (data: UnionSearchResponse<T>) => void;
+}
+
+export interface MultiSearchResultsStreamConfig<T extends DocumentSchema[]>
+  extends BaseStreamConfig {
+  onComplete?: (data: {
+    results: { [Index in keyof T]: SearchResponse<T[Index]> } & {
+      length: T["length"];
+    };
+  }) => void;
+}
+
+type CommonMultiSearchParametersBase<T extends DocumentSchema> = Partial<
+  BaseMultiSearchRequestSchema & Omit<SearchParams<T>, "streamConfig">
+>;
+
+export type MultiSearchUnionParameters<T extends DocumentSchema> =
+  CommonMultiSearchParametersBase<T> & {
+    streamConfig?: MultiSearchUnionStreamConfig<T>;
+    use_cache?: boolean;
+  };
+
+export type MultiSearchResultsParameters<T extends DocumentSchema[]> =
+  CommonMultiSearchParametersBase<T[number]> & {
+    streamConfig?: MultiSearchResultsStreamConfig<T>;
+    use_cache?: boolean;
+  };
 
 export default class MultiSearch {
   private requestWithCache: RequestWithCache;
@@ -83,13 +114,13 @@ export default class MultiSearch {
 
   async perform<const T extends DocumentSchema[] = []>(
     searchRequests: MultiSearchRequestsWithUnionSchema<T[number]>,
-    commonParams?: Partial<MultiSearchRequestSchema<T[number]>>,
+    commonParams?: MultiSearchUnionParameters<T[number]>,
     options?: { cacheSearchResultsForSeconds?: number },
   ): Promise<UnionSearchResponse<T[number]>>;
 
   async perform<const T extends DocumentSchema[] = []>(
     searchRequests: MultiSearchRequestsWithoutUnionSchema<T[number]>,
-    commonParams?: Partial<MultiSearchRequestSchema<T[number]>>,
+    commonParams?: MultiSearchResultsParameters<T>,
     options?: { cacheSearchResultsForSeconds?: number },
   ): Promise<{
     results: { [Index in keyof T]: SearchResponse<T[Index]> } & {
@@ -99,33 +130,34 @@ export default class MultiSearch {
 
   async perform<const T extends DocumentSchema[] = []>(
     searchRequests: MultiSearchRequestsSchema<T[number]>,
-    commonParams: Partial<MultiSearchRequestSchema<T[number]>> = {},
+    commonParams?:
+      | MultiSearchUnionParameters<T[number]>
+      | MultiSearchResultsParameters<T>,
     {
       cacheSearchResultsForSeconds = this.configuration
         .cacheSearchResultsForSeconds,
     }: { cacheSearchResultsForSeconds?: number } = {},
   ): Promise<MultiSearchResponse<T>> {
-    const additionalHeaders = {};
-    if (this.useTextContentType) {
-      additionalHeaders["content-type"] = "text/plain";
-    }
+    const params = { ...commonParams } as
+      | MultiSearchUnionParameters<T[number]>
+      | MultiSearchResultsParameters<T>
+      | { use_cache?: boolean; streamConfig?: unknown };
 
-    const additionalQueryParams = {};
     if (this.configuration.useServerSideSearchCache === true) {
-      additionalQueryParams["use_cache"] = true;
+      params.use_cache = true;
     }
 
-    const queryParams = { ...commonParams, ...additionalQueryParams };
-
-    const { extractedStreamConfig, searchesWithoutStreamConfig } =
-      this.extractStreamConfig(searchRequests.searches);
-
-    const normalizedSearchRequests = {
-      ...searchRequests,
-      searches: searchesWithoutStreamConfig.map(normalizeArrayableParams),
+    const normalizedSearchRequests: Omit<typeof searchRequests, "searches"> & {
+      searches: ExtractBaseTypes<SearchParams<T[number]>>[];
+    } = {
+      union: searchRequests.union,
+      searches: searchRequests.searches.map(normalizeArrayableParams),
     };
 
-    const normalizedQueryParams = normalizeArrayableParams(queryParams);
+    const { streamConfig, ...paramsWithoutStream } = params;
+    const normalizedQueryParams = normalizeArrayableParams(
+      paramsWithoutStream as SearchParams<T[number]>,
+    );
 
     return this.requestWithCache.perform(
       this.apiCall,
@@ -134,43 +166,17 @@ export default class MultiSearch {
         path: RESOURCEPATH,
         body: normalizedSearchRequests,
         queryParams: normalizedQueryParams,
-        headers: additionalHeaders,
-        streamConfig: extractedStreamConfig,
-        isStreamingRequest: this.isStreamingRequest(searchRequests),
+        headers: this.useTextContentType
+          ? { "content-type": "text/plain" }
+          : {},
+        streamConfig,
+        isStreamingRequest: this.isStreamingRequest(params),
       },
       { cacheResponseForSeconds: cacheSearchResultsForSeconds },
     );
   }
 
-  private isStreamingRequest<T extends DocumentSchema>(
-    searchRequests: MultiSearchRequestsSchema<T>,
-  ) {
-    return searchRequests.searches.some(
-      (search) => search.streamConfig !== undefined,
-    );
-  }
-
-  /**
-   * Extracts streamConfig from search requests and returns both the config and clean requests
-   */
-  private extractStreamConfig<T extends DocumentSchema>(
-    searches: (
-      | MultiSearchRequestSchema<T>
-      | MultiSearchRequestWithPresetSchema<T>
-    )[],
-  ) {
-    const searchWithStreamConfig = searches.find(
-      (search) => search.streamConfig !== undefined,
-    );
-
-    const searchesWithoutStreamConfig = searches.map(
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      ({ streamConfig, ...rest }) => rest,
-    );
-
-    return {
-      extractedStreamConfig: searchWithStreamConfig?.streamConfig,
-      searchesWithoutStreamConfig,
-    };
+  private isStreamingRequest(commonParams: { streamConfig?: unknown }) {
+    return commonParams.streamConfig !== undefined;
   }
 }
