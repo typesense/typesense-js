86a7dd586421911de9937c8e2fb012ba106b33d0
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-18 16:37:23 +0300

    feat(synonyms): add SynonymSets class for SynonymsV2 API
    - implement SynonymSets class with retrieve method
    - add SynonymItemSchema and SynonymSetSchema interfaces
    - support for new /synonym_sets endpoint
    - prepare for root-level synonym set management

---
+++ src/Typesense/SynonymSets.ts
@@ -0,0 +1,30 @@
+import ApiCall from "./ApiCall";
+
+export interface SynonymItemSchema {
+  id: string;
+  synonyms: string[];
+  root?: string;
+  locale?: string;
+  symbols_to_index?: string[];
+}
+
+export interface SynonymSetCreateSchema {
+  synonyms: SynonymItemSchema[];
+}
+
+export interface SynonymSetSchema extends SynonymSetCreateSchema {
+  name: string;
+}
+
+export interface SynonymSetsRetrieveSchema {
+  synonym_sets: SynonymSetSchema[];
+}
+
+export default class SynonymSets {
+  constructor(private apiCall: ApiCall) {}
+  static readonly RESOURCEPATH = "/synonym_sets";
+
+  async retrieve(): Promise<SynonymSetSchema[]> {
+    return this.apiCall.get<SynonymSetSchema[]>(SynonymSets.RESOURCEPATH);
+  }
+}
