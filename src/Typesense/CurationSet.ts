733a8fd5b988b442df36a80089e0bcd9e21567a2
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-09-29 20:37:03 +0300

    chore: format

--- src/Typesense/CurationSet.ts
+++ src/Typesense/CurationSet.ts
@@ -12,7 +12,10 @@ export default class CurationSet {
   private readonly _items: CurationSetItems;
   private individualItems: Record<string, CurationSetItem> = {};
 
-  constructor(private name: string, private apiCall: ApiCall) {
+  constructor(
+    private name: string,
+    private apiCall: ApiCall,
+  ) {
     this._items = new CurationSetItems(this.name, apiCall);
   }
 
@@ -51,5 +54,3 @@ export default class CurationSet {
     return `${CurationSets.RESOURCEPATH}/${encodeURIComponent(this.name)}`;
   }
 }
-
-
