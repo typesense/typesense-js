083360563c95ad53c13d68dff5281229784e26e8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-05-19 12:33:24 +0300

    docs(documents): add jsdoc for document class methods

--- src/Typesense/Document.ts
+++ src/Typesense/Document.ts
@@ -19,16 +19,40 @@ export class Document<T extends DocumentSchema = object> {
     private apiCall: ApiCall
   ) {}
 
+  /**
+   * Fetch an individual document from a collection by using its ID.
+   *
+   * @example
+   * await client.collections("products").documents("1").retrieve()
+   *
+   * @see https://typesense.org/docs/latest/api/documents.html#retrieve-a-document
+   */
   async retrieve(options?: DocumentsRetrieveParameters): Promise<T> {
     const queryParams = normalizeArrayableParams(options ?? {})
-    
+
     return this.apiCall.get<T>(this.endpointPath(), queryParams);
   }
 
+  /**
+   * Delete an individual document from a collection by using its ID.
+   *
+   * @example
+   * await client.collections("products").documents("1").delete()
+   *
+   * @see https://typesense.org/docs/latest/api/documents.html#delete-a-document
+   */
   async delete(options?: DeleteQuery): Promise<T> {
     return this.apiCall.delete<T>(this.endpointPath(), options);
   }
 
+  /**
+   * Update an individual document by ID by merging the provided fields.
+   *
+   * @example
+   * await client.collections("products").documents("1").update({ in_stock: true })
+   *
+   * @see https://typesense.org/docs/latest/api/documents.html#update-a-document
+   */
   async update(
     partialDocument: Partial<T>,
     options: DocumentWriteParameters = {}
